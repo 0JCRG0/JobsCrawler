faa269c1162d9e0be11e99ced822c3b657fc8f6c
JCRG <juancarlosrg1999@gmail.com>
2023-12-08 03:08:12 -0600

    fixing follow link

---
+++ resources/bs4_resources/lil_scrapper.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url_to_follow = "https://echojobs.io/job/wasabi-technologies-senior-storage-engineer-g88xl"
+#selector = 'div[data-qa="btn-apply-bottom"] a'
+
+#url_to_follow = "https://echojobs.io/job/airbnb-senior-backend-engineer-media-foundation-dtjyb"
+selector = 'job-detail mb-4'
+
+def FollowLinkEchoJobs(url_to_follow: str, selector: str) -> str:
+
+	
+	# Make a request to the website
+	r = requests.get(url_to_follow)
+	r.content
+
+	# Use the 'html.parser' to parse the page
+	soup = BeautifulSoup(r.content, 'html.parser')
+	
+	# Find the 'div' tag with the class "job-detail mb-4"
+	div_tag = soup.find('div', {'class': selector})
+
+	# Get the text of the 'div' tag
+	text = div_tag.get_text()
+
+	# Print the text
+	print(text)
+	
+	#text = soup.get_text()
+
+	#print(text)
+
+	# Find the 'a' tag under the 'div' with the 'data-qa' attribute of 'btn-apply-bottom'
+	#job_detail = soup.select_one(selector)
+
+	# Get the value of the 'href' attribute
+	#url = a_tag.get('href')
+
+	# Use regular expression to remove "/apply" from the URL
+	#modified_url = re.sub(r'/apply$', '', url)
+
+	# Print the URL
+	#print(modified_url)
+	# Send a GET request to the URL
+	#r = requests.get(modified_url)
+
+	# Parse the HTML content of the page with BeautifulSoup
+	#soup = BeautifulSoup(r.content, 'html.parser')
+
+	# Get the whole text of the page
+	#text = soup.get_text()
+
+	# Print the text
+	#print(text)
+
+FollowLinkEchoJobs(url_to_follow, selector)
+
+exit(0)
+
+
+s = "https:\\u002F\\u002Fjobs.lever.co\\u002Fwasabi\\u002F4c6f5c9b-0876-405e-9e8a-9c374b7295f7\\u002Fapply"
+s = s.encode('unicode_escape').decode('unicode_escape')
+print(s)
+
+s = "https:\\u002F\\u002Fjobs.lever.co\\u002Fwasabi\\u002F4c6f5c9b-0876-405e-9e8a-9c374b7295f7\\u002Fapply"
+s = codecs.decode(s, 'unicode_escape')
+print(s)
+

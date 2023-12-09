7ade7c0af00ea655343a3a6bfacb04de6f080505
JCRG <juancarlosrg1999@gmail.com>
2023-12-09 04:12:28 -0600

    another refactor

--- utils/handy.py
+++ utils/handy.py
@@ -1,11 +1,8 @@
-import re 
 import psycopg2
 from psycopg2 import sql
 import logging
-import json
-import pandas as pd
-import numpy as np
 import os
+from typing import Callable
 from dotenv import load_dotenv
 
 
@@ -13,11 +10,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-user = os.environ.get('user')
-password = os.environ.get('password')
-host = os.environ.get('host')
-port = os.environ.get('port')
-database = os.environ.get('database')
+LOCAL_POSTGRE_URL = os.environ.get("LOCAL_POSTGRE_URL")
+RENDER_POSTGRE_URL = os.environ.get("RENDER_POSTGRE_URL")
+
 
 
 """ LOAD THE ENVIRONMENT VARIABLES """
@@ -32,90 +27,86 @@ def LoggingMasterCrawler():
 						level=logging.INFO,
 						format=log_format)
 
-""" CLEANING FUNCTIONS"""
-def clean_rows(s):
-	if not isinstance(s, str):
-		print(f"{s} is not a string! Returning unmodified")
-		return s
-	s = re.sub(r'{', '', s)
-	s = re.sub(r'}', '', s)
-	s = re.sub(r'[\[\]]', '', s)
-	s = re.sub(r"'", '', s)
-	s = re.sub(r",", ' ', s)
-	return s
-
-def cleansing_selenium_crawlers(s):
-	if not isinstance(s, str):
-		print(f"{s} is not a string! Returning unmodified")
-		return s
-	s = " ".join(s.split())
-	s = re.sub(r'<[^>]+>', '', s)
-	s = re.sub(r'n/', '', s)
-	s = re.sub(r'"', '', s)
-	s = re.sub(r'{', '', s)
-	s = re.sub(r'}', '', s)
-	s = re.sub(r'[\[\]]', '', s)
-	s = re.sub(r"'", '', s)
-	return s
-
-	# Handy cleansing function
-
-def bye_regex(s):
-	# Remove leading/trailing white space
-	s = s.strip()
-		# Replace multiple spaces with a single space
-	s = re.sub(r'\s+', ' ', s)
-		# Remove newline characters
-	s = re.sub(r'\n', '', s)
-		# Replace regex for í
-	s = re.sub(r'√≠', 'í', s)
-		# Replace word
-	s = re.sub(r'Posted', '', s)
-		# Remove HTML tags
-	s = re.sub(r'<.*?>', '', s)
-
-#CLEANING FUNCTIONS FOR RSS' crawlers
-def clean_link_rss(s):
-	# Remove leading/trailing white space
-	s = s.strip()
-		
-	# Replace multiple spaces with a single space
-	s = re.sub(r'\s+', ' ', s)
-		
-	# Remove newline characters
-	s = re.sub(r'\n', '', s)
-		
-	return s
-
-def clean_other_rss(s):
-	if not isinstance(s, str):
-		return s
-	# Remove leading/trailing white space
-	s = s.strip()
-		
-	# Replace multiple spaces with a single space
-	s = re.sub(r'\s+', ' ', s)
-		
-	# Remove newline characters
-	s = re.sub(r'\n', '', s)
-		
-	# Remove HTML tags
-	s = re.sub(r'<.*?>', '', s)
-		
-	# Remove non-alphanumeric characters (except for spaces)
-	s = re.sub(r'[^a-zA-Z0-9\s]+', '', s)
-		
-	# Remove symbols
-	s = re.sub(r'[-–—•@Ôªø]+', '', s)
-			
-	return s
-
 """ POSTGRE FUNCTIONS """
-
-
-def to_postgre(df):
+def to_render_postgre(df):
 	# create a connection to the PostgreSQL database
-	cnx = psycopg2.connect(user='postgres', password='3312', host='localhost', port='5432', database='postgres')
+	cnx = psycopg2.connect(RENDER_POSTGRE_URL)
+
+	# create a cursor object
+	cursor = cnx.cursor()
+
+	# execute the initial count query and retrieve the result
+	initial_count_query = '''
+		SELECT COUNT(*) FROM main_jobs
+	'''
+	cursor.execute(initial_count_query)
+	initial_count_result = cursor.fetchone()
+	
+	""" IF THERE IS A DUPLICATE LINK IT SKIPS THAT ROW & DOES NOT INSERTS IT
+		IDs ARE ENSURED TO BE UNIQUE BCOS OF THE SERIAL ID THAT POSTGRE MANAGES AUTOMATICALLY
+	"""
+	jobs_added = []
+	for index, row in df.iterrows():
+		insert_query = '''
+			INSERT INTO main_jobs (title, link, description, pubdate, location, timestamp)
+			VALUES (%s, %s, %s, %s, %s, %s)
+			ON CONFLICT (link) DO NOTHING
+			RETURNING *
+		'''
+		values = (row['title'], row['link'], row['description'], row['pubdate'], row['location'], row['timestamp'])
+		cursor.execute(insert_query, values)
+		affected_rows = cursor.rowcount
+		if affected_rows > 0:
+			jobs_added.append(cursor.fetchone())
+
+
+	""" LOGGING/PRINTING RESULTS"""
+
+	final_count_query = '''
+		SELECT COUNT(*) FROM main_jobs
+	'''
+	# execute the count query and retrieve the result
+	cursor.execute(final_count_query)
+	final_count_result = cursor.fetchone()
+
+	# calculate the number of unique jobs that were added
+	if initial_count_result is not None:
+		initial_count = initial_count_result[0]
+	else:
+		initial_count = 0
+	jobs_added_count = len(jobs_added)
+	if final_count_result is not None:
+		final_count = final_count_result[0]
+	else:
+		final_count = 0
+
+	# check if the result set is not empty
+	print("\n")
+	print("to_render_postgre() ON main_jobs report:", "\n")
+	print(f"Total count of jobs before crawling: {initial_count}")
+	print(f"Total number of unique jobs: {jobs_added_count}")
+	print(f"Current total count of jobs in PostgreSQL: {final_count}")
+
+	postgre_report = "to_render_postgre() ON main_jobs report:"\
+					"\n"\
+					f"Total count of jobs before crawling: {initial_count}" \
+					"\n"\
+					f"Total number of unique jobs: {jobs_added_count}" \
+					"\n"\
+					f"Current total count of jobs in PostgreSQL: {final_count}"
+
+	logging.info(postgre_report)
+
+	# commit the changes to the database
+	cnx.commit()
+
+	# close the cursor and connection
+	cursor.close()
+	cnx.close()
+
+def to_local_postgre(df):
+	# create a connection to the PostgreSQL database
+	cnx = psycopg2.connect(LOCAL_POSTGRE_URL)
 
 	# create a cursor object
 	cursor = cnx.cursor()
@@ -189,9 +180,9 @@ def to_postgre(df):
 	cursor.close()
 	cnx.close()
 
-def test_postgre(df):
+def to_local_test(df):
 	# create a connection to the PostgreSQL database
-	cnx = psycopg2.connect(user='postgres', password='3312', host='localhost', port='5432', database='postgres')
+	cnx = psycopg2.connect(LOCAL_POSTGRE_URL)
 
 	# create a cursor object
 	cursor = cnx.cursor()
@@ -264,9 +255,18 @@ def test_postgre(df):
 	cursor.close()
 	cnx.close()
 
-async def link_exists_in_db(link, cur):
+async def link_exists_in_db(link, cur, pipeline):
+	
+	table = None
 
-	query = sql.SQL("SELECT EXISTS(SELECT 1 FROM test WHERE link=%s)")
+	if pipeline == "PROD":
+		table = "main_jobs"
+	elif pipeline == "LocalProd":
+		table = "main_jobs"
+	else:
+		table = "test"
+
+	query = sql.SQL(f"SELECT EXISTS(SELECT 1 FROM {table} WHERE link=%s)")
 	cur.execute(query, (link,))
 
 	# Fetch the result
@@ -294,70 +294,27 @@ def class_json_strategy(data, elements_path, class_json):
 		jobs = data
 		return jobs
 
-## Function to clean api_pubdates
-def api_pubdate(s):
-	if s is not None:
-		pubdate = s[0:10]
-		return pubdate
 
-# FUNCTIONS FOR FORMATTING PUBDATES IN RSS
-def adby_pubdate(s):
-	#slice the jobs that are formatted as %a %d %b %Y
-	if s is not None:
-		if not s.startswith("20"):
-			s_sliced = s[0:15]
-			return s_sliced
-
-def YMD_pubdate(s):
-	#slice the jobs that are formatted as %a %d %b %Y
-	if s is not None:
-		YMD_pub = s[0:8]
-		return YMD_pub
-
-#Cleaning function indeed
-def indeed_regex(s):
-		pattern = r'<li>(.*?)<\/li>'
-		matches = re.findall(pattern, s, re.DOTALL)
-		if len(matches) > 0:
-			text = matches[0]
-			text = re.sub(r'<b>|<\/b>', '', text)
-			return text
-		else:
-			return ''
-
-def postgre_data(table_name:str = "main_jobs"):
-	conn = psycopg2.connect(user='postgres', password='3312', host='localhost', port='5432', database='postgres')
-
-	# Create a cursor object
-	cur = conn.cursor()
-
-	# Fetch new data from the table where id is greater than max_id
-	cur.execute(f"SELECT * FROM {table_name}")
-	new_data = cur.fetchall()
-
-
-	# Close the database connection
-	conn.commit()
-	cur.close()
-	conn.close()
+def test_or_prod(
+		pipeline: str,
+		json_prod: str,
+		json_test:str,
+		render_postgre_prod: Callable = to_render_postgre,
+		local_postgre_prod: Callable = to_local_postgre,
+		local_postgre_test: Callable = to_local_test,
+		local_url_postgre: str = LOCAL_POSTGRE_URL,
+		render_url_postgre: str = RENDER_POSTGRE_URL,):
 	
-	# Separate the columns into individual lists
-	ids = [row[0] for row in new_data]
-	titles = [row[1] for row in new_data]
-	descriptions = [row[2] for row in new_data]
-	locations = [row[3] for row in new_data]
-
-
-	return ids, titles, locations, descriptions
-
-def test_or_prod(pipeline: str, json_prod: str, json_test:str, postgre_prod, postgre_test):
-	if pipeline and json_prod and json_test and postgre_prod and postgre_test:
-		if pipeline == 'MAIN':
-			print("\n", f"Pipeline is set to 'MAIN'. Jobs will be sent to PostgreSQL's main_jobs table", "\n")
-			return json_prod or "", postgre_prod or ""
+	if pipeline and json_prod and json_test and render_postgre_prod and local_postgre_prod and local_postgre_test and local_url_postgre and render_url_postgre:
+		if pipeline == 'PROD':
+			print("\n", f"Pipeline is set to 'PROD'. Jobs will be sent to Render PostgreSQL's main_jobs table", "\n")
+			return json_prod or "", render_postgre_prod or "", render_url_postgre or ""
+		elif pipeline == 'LocalProd':
+			print("\n", f"Pipeline is set to 'LocalProd'. Jobs will be sent to Local PostgreSQL's main_jobs table", "\n")
+			return json_prod or "", local_postgre_prod or "", local_url_postgre or ""
 		elif pipeline == 'TEST':
 			print("\n", f"Pipeline is set to 'TEST'. Jobs will be sent to PostgreSQL's test table", "\n")
-			return json_test or "", postgre_test or ""
+			return json_test or "", local_postgre_test or "", local_url_postgre or ""
 		else:
 			print("\n", "Incorrect argument! Use either 'MAIN' or 'TEST' to run this script.", "\n")
 			logging.error("Incorrect argument! Use either 'MAIN' or 'TEST' to run this script.")

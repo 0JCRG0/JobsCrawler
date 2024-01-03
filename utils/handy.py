f825e056d583eaa939a1db397d4b69ab5228c3e4
JCRG <juancarlosrg1999@gmail.com>
2024-01-03 12:07:13 -0600

    changed DB URL -> now sent to DO

--- utils/handy.py
+++ utils/handy.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 LOCAL_POSTGRE_URL = os.environ.get("LOCAL_POSTGRE_URL")
 RENDER_POSTGRE_URL = os.environ.get("RENDER_POSTGRE_URL")
-
+DATABASE_URL = os.environ.get("DATABASE_URL_DO")
 
 
 """ LOAD THE ENVIRONMENT VARIABLES """
@@ -28,9 +28,9 @@ def LoggingMasterCrawler():
 						format=log_format)
 
 """ POSTGRE FUNCTIONS """
-def to_render_postgre(df):
+def to_managed_prod_postgre(df):
 	# create a connection to the PostgreSQL database
-	cnx = psycopg2.connect(RENDER_POSTGRE_URL)
+	cnx = psycopg2.connect(DATABASE_URL)
 
 	# create a cursor object
 	cursor = cnx.cursor()
@@ -82,12 +82,88 @@ def to_render_postgre(df):
 
 	# check if the result set is not empty
 	print("\n")
-	print("to_render_postgre() ON main_jobs report:", "\n")
+	print("to_managed_prod_postgre() ON main_jobs report:", "\n")
 	print(f"Total count of jobs before crawling: {initial_count}")
 	print(f"Total number of unique jobs: {jobs_added_count}")
 	print(f"Current total count of jobs in PostgreSQL: {final_count}")
 
-	postgre_report = "to_render_postgre() ON main_jobs report:"\
+	postgre_report = "to_managed_prod_postgre() ON main_jobs report:"\
+					"\n"\
+					f"Total count of jobs before crawling: {initial_count}" \
+					"\n"\
+					f"Total number of unique jobs: {jobs_added_count}" \
+					"\n"\
+					f"Current total count of jobs in PostgreSQL: {final_count}"
+
+	logging.info(postgre_report)
+
+	# commit the changes to the database
+	cnx.commit()
+
+	# close the cursor and connection
+	cursor.close()
+	cnx.close()
+
+def to_managed_test_postgre(df):
+	# create a connection to the PostgreSQL database
+	cnx = psycopg2.connect(DATABASE_URL)
+
+	# create a cursor object
+	cursor = cnx.cursor()
+
+	# execute the initial count query and retrieve the result
+	initial_count_query = '''
+		SELECT COUNT(*) FROM test
+	'''
+	cursor.execute(initial_count_query)
+	initial_count_result = cursor.fetchone()
+	
+	""" IF THERE IS A DUPLICATE LINK IT SKIPS THAT ROW & DOES NOT INSERTS IT
+		IDs ARE ENSURED TO BE UNIQUE BCOS OF THE SERIAL ID THAT POSTGRE MANAGES AUTOMATICALLY
+	"""
+	jobs_added = []
+	for index, row in df.iterrows():
+		insert_query = '''
+			INSERT INTO test (title, link, description, pubdate, location, timestamp)
+			VALUES (%s, %s, %s, %s, %s, %s)
+			ON CONFLICT (link) DO NOTHING
+			RETURNING *
+		'''
+		values = (row['title'], row['link'], row['description'], row['pubdate'], row['location'], row['timestamp'])
+		cursor.execute(insert_query, values)
+		affected_rows = cursor.rowcount
+		if affected_rows > 0:
+			jobs_added.append(cursor.fetchone())
+
+
+	""" LOGGING/PRINTING RESULTS"""
+
+	final_count_query = '''
+		SELECT COUNT(*) FROM test
+	'''
+	# execute the count query and retrieve the result
+	cursor.execute(final_count_query)
+	final_count_result = cursor.fetchone()
+
+	# calculate the number of unique jobs that were added
+	if initial_count_result is not None:
+		initial_count = initial_count_result[0]
+	else:
+		initial_count = 0
+	jobs_added_count = len(jobs_added)
+	if final_count_result is not None:
+		final_count = final_count_result[0]
+	else:
+		final_count = 0
+
+	# check if the result set is not empty
+	print("\n")
+	print("to_managed_test_postgre() ON test report:", "\n")
+	print(f"Total count of jobs before crawling: {initial_count}")
+	print(f"Total number of unique jobs: {jobs_added_count}")
+	print(f"Current total count of jobs in PostgreSQL: {final_count}")
+
+	postgre_report = "to_managed_test_postgre() ON test report:"\
 					"\n"\
 					f"Total count of jobs before crawling: {initial_count}" \
 					"\n"\
@@ -299,21 +375,25 @@ def test_or_prod(
 		pipeline: str,
 		json_prod: str,
 		json_test:str,
-		render_postgre_prod: Callable = to_render_postgre,
+		to_managed_prod_postgre: Callable = to_managed_prod_postgre,
+		to_managed_test_postgre: Callable = to_managed_test_postgre,
 		local_postgre_prod: Callable = to_local_postgre,
 		local_postgre_test: Callable = to_local_test,
 		local_url_postgre: str = LOCAL_POSTGRE_URL,
-		render_url_postgre: str = RENDER_POSTGRE_URL,):
+		managed_postgre_url: str = DATABASE_URL,):
 	
-	if pipeline and json_prod and json_test and render_postgre_prod and local_postgre_prod and local_postgre_test and local_url_postgre and render_url_postgre:
+	if pipeline and json_prod and json_test and to_managed_prod_postgre and to_managed_test_postgre and local_postgre_prod and local_postgre_test and local_url_postgre and managed_postgre_url:
 		if pipeline == 'PROD':
-			print("\n", f"Pipeline is set to 'PROD'. Jobs will be sent to Render PostgreSQL's main_jobs table", "\n")
-			return json_prod or "", render_postgre_prod or "", render_url_postgre or ""
+			print("\n", f"Pipeline is set to 'PROD'. Jobs will be sent to DO PostgreSQL. Expect jobs in main_jobs table", "\n")
+			return json_prod or "", to_managed_prod_postgre or "", managed_postgre_url or ""
+		elif pipeline == 'TEST':
+			print("\n", f"Pipeline is set to 'TEST'. Jobs will be sent to DO's PostgreSQL. Expect jobs in test table", "\n")
+			return json_test or "", to_managed_test_postgre or "", managed_postgre_url or ""
 		elif pipeline == 'LocalProd':
 			print("\n", f"Pipeline is set to 'LocalProd'. Jobs will be sent to Local PostgreSQL's main_jobs table", "\n")
 			return json_prod or "", local_postgre_prod or "", local_url_postgre or ""
-		elif pipeline == 'TEST':
-			print("\n", f"Pipeline is set to 'TEST'. Jobs will be sent to PostgreSQL's test table", "\n")
+		elif pipeline == 'LocalTest':
+			print("\n", f"Pipeline is set to 'LocalProd'. Jobs will be sent to Local PostgreSQL's main_jobs table", "\n")
 			return json_test or "", local_postgre_test or "", local_url_postgre or ""
 		else:
 			print("\n", "Incorrect argument! Use either 'MAIN' or 'TEST' to run this script.", "\n")

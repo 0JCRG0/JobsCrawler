c601b24d72455ea0f477b4ba809706f3deefb2c6
root <juancarlosrg1999@gmail.com>
2025-04-03 03:12:37 +0000

    script for sending logs to discord

--- logs.sh
+++ logs.sh
@@ -6,7 +6,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # Change to the correct directory
 cd /root/JobsCrawler || exit
 
-echo "Starting 'main.py' at $(date)" >> /root/JobsCrawler/logs/main_logger.log
+echo "Starting log script at $(date)" >> /root/JobsCrawler/logs/logger.log
 
 # Load environment variables
 set -a
@@ -17,9 +17,9 @@ set +a
 source /root/JobsCrawler/venv/bin/activate
 
 # Run the Python script and redirect its output to a log file
-python /root/JobsCrawler/src/main.py >> /root/JobsCrawler/logs/script_output.log 2>&1
+python /root/JobsCrawler/src/logs_in_discord.py >> /root/JobsCrawler/logs/logs_output.log 2>&1
 
 # Deactivate virtual environment
 deactivate
 
-echo "Finished script at $(date)" >> /root/JobsCrawler/logs/main_logger.log
+echo "Finished script at $(date)" >> /root/JobsCrawler/logs/logger.log
